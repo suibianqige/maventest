606791c3a31dbef0c4ea4e79ed4191301e099141
suibianqige <1564104849@qq.com>
2017-07-18 14:29:15 +0800

    通过github托管项目

---
+++ maventest/src/main/java/com/yang/IndexAction.java
@@ -0,0 +1,14 @@
+package com.yang;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+	
+}
